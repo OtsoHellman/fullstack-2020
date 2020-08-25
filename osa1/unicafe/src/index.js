a358587769e7674b7a2541624e99dc5fac9f198a
otsohellman <otso.hellman@gmail.com>
2020-08-25 23:01:07 +0300

    1-6

---
+++ osa1/unicafe/src/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const Button = (props) => (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+  const Statistic = (props) => <p>{props.name} {props.amount}</p>
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <h1>statistics</h1>
+      <Statistic name={"good"} amount={good} />
+      <Statistic name={"neutral"} amount={neutral} />
+      <Statistic name={"bad"} amount={bad} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,
+  document.getElementById('root')
+)
