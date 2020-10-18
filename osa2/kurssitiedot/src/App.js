c55aa860b3f37c78ab1c52d6e8b48ecaeebef224
otsohellman <otso.hellman@gmail.com>
2020-10-18 22:58:03 +0300

    2-2

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -16,47 +16,45 @@ const Total = ({ parts }) => (
         .reduce((a, b) => a + b)}</p>
 )
 
-const Course = ({course}) => {
+const Course = ({ course }) => {
     return (
         <div>
             <Header course={course.name} />
             <Content contents={course.parts}
             />
-            {
-                //<Total parts={course.parts} />
-            }
+            <Total parts={course.parts} />
         </div>
     )
 }
 
 const App = () => {
     const course = {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        }
-      ]
+        name: 'Half Stack application development',
+        id: 1,
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10,
+                id: 1
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7,
+                id: 2
+            },
+            {
+                name: 'State of a component',
+                exercises: 14,
+                id: 3
+            }
+        ]
     }
-  
+
     return (
-      <div>
-        <Course course={course} />
-      </div>
+        <div>
+            <Course course={course} />
+        </div>
     )
-  }
+}
 
 export default App
