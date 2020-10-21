8e3444f6c7cf3e27f1c0dbecba03547ccbe0d443
otsohellman <otso.hellman@gmail.com>
2020-10-21 18:56:26 +0300

    2-11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,17 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import PersonForm from './components/PersonForm'
 import PersonList from './components/PersonList'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [filterValue, setFilterValue] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+
+  useEffect(()=> {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data))
+  },[])
 
   const handleFilterChange = (event) => setFilterValue(event.target.value)
   const handlePersonChange = (event) => setNewName(event.target.value)
