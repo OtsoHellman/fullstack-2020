5e24fe9231e6a31037c158648ed050a34a92bb95
otsohellman <otso.hellman@gmail.com>
2020-10-24 01:44:32 +0300

    4-5

--- osa4/blogilista/utils/listHelper.js
+++ osa4/blogilista/utils/listHelper.js
@@ -2,6 +2,10 @@ const dummy = (blogs) => 1
 
 const totalLikes = (blogs) => blogs
     .map(blog => blog.likes)
-    .reduce((a,b) => a+b, 0)
+    .reduce((a, b) => a + b, 0)
 
-export default { dummy, totalLikes }
+const favoriteBlog = (blogs) => blogs.length > 0
+    ? blogs.reduce((bestBlog, blog) => bestBlog.likes > blog.likes ? bestBlog : blog)
+    : null
+
+export default { dummy, totalLikes, favoriteBlog }
