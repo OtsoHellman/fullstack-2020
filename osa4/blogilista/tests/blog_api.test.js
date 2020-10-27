8de0098ad670e18255fdb3526a8a9fae97946f04
otsohellman <otso.hellman@gmail.com>
2020-10-27 20:54:37 +0200

    4-11

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -6,49 +6,64 @@ import Blog from '../models/blog.js'
 const api = supertest(app)
 
 const blogs = [
-    { _id: "5a422a851b54a676234d17f7", title: "React patterns", author: "Michael Chan", url: "https://reactpatterns.com/", likes: 7, __v: 0 },
-    { _id: "5a422aa71b54a676234d17f8", title: "Go To Statement Considered Harmful", author: "Edsger W. Dijkstra", url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html", likes: 5, __v: 0 },
-    { _id: "5a422b3a1b54a676234d17f9", title: "Canonical string reduction", author: "Edsger W. Dijkstra", url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html", likes: 12, __v: 0 },
-    { _id: "5a422b891b54a676234d17fa", title: "First class tests", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll", likes: 10, __v: 0 },
-    { _id: "5a422ba71b54a676234d17fb", title: "TDD harms architecture", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html", likes: 0, __v: 0 }
+    { title: "React patterns", author: "Michael Chan", url: "https://reactpatterns.com/", likes: 7 },
+    { title: "Go To Statement Considered Harmful", author: "Edsger W. Dijkstra", url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html", likes: 5 },
+    { title: "Canonical string reduction", author: "Edsger W. Dijkstra", url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html", likes: 12 },
+    { title: "First class tests", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll", likes: 10 },
+    { title: "TDD harms architecture", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html", likes: 0 }
 ]
 
-const newBlog = { _id: "5a422bc61b54a676234d17fc", title: "Type wars", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html", likes: 2, __v: 0 }
+const newBlog = { title: "Type wars", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html" }
 
 beforeEach(async () => {
     await Blog.deleteMany({})
     await Blog.insertMany(blogs)
 })
 
-test('blogs are returned as json', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
+
+describe('blogs', () => {
+
+
+    test('are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
+
+    test('returns correct number of items', async () => {
+        const response = await api.get('/api/blogs')
+        expect(response.body).toHaveLength(blogs.length)
+
+    })
+
+    test('have an id field', async () => {
+        const response = await api.get('/api/blogs')
+        response.body.forEach(blog => expect(blog.id).toBeDefined())
+    })
+
+    test('can be added to database', async () => {
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+
+        const response = await api.get('/api/blogs')
+        expect(response.body).toHaveLength(blogs.length + 1)
+    })
+
+    test('have 0 likes unless otherwise specified', async () => {
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+
+        const response = await api.get('/api/blogs')
+        expect(response.body.find(blog => blog.title === "Type wars").likes).toEqual(0)
+    })
 })
 
-test('correct number of blogs are returned', async () => {
-    const response = await api.get('/api/blogs')
-    expect(response.body).toHaveLength(blogs.length)
-
-})
-
-test('blogs have id field', async () => {
-    const response = await api.get('/api/blogs')
-    response.body.forEach(blog => expect(blog.id).toBeDefined())
-})
-
-test('blogs can be added to database', async () => {
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-
-
-    const response = await api.get('/api/blogs')
-    expect(response.body).toHaveLength(blogs.length + 1)
-})
 
 afterAll(() => {
     mongoose.connection.close()
