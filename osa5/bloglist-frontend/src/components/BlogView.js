7e32a93fedf195e082ae3c29355d1c6256ca4b2e
otsohellman <otso.hellman@gmail.com>
2020-10-29 19:33:19 +0200

    5-5

--- osa5/bloglist-frontend/src/components/BlogView.js
+++ osa5/bloglist-frontend/src/components/BlogView.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import BlogList from './BlogList'
 import BlogCreateForm from './BlogCreateForm'
+import Togglable from './Togglable'
 
 export default ({ blogs, name, handleLogout, postBlog }) => <div>
     <h2>blogs</h2>
     {name}
     <button onClick={handleLogout}>log out</button>
-    <BlogCreateForm postBlog={postBlog} />
+    <Togglable buttonLabel="add blog">
+        <BlogCreateForm postBlog={postBlog} />
+    </Togglable>
     <BlogList blogs={blogs} />
 </div>
