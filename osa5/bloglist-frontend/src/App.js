9702a3d51f0a0bc75b21d36ada26ecfb16bea7ef
otsohellman <otso.hellman@gmail.com>
2020-10-29 18:54:31 +0200

    5-4

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react'
-import BlogView from './components/BlogView'
-import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import Notification from './components/Notification'
+import BlogView from './components/BlogView'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null)
+  const [notificationMessage, setNotificationMessage] = useState({ message: null, isError: null })
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -34,21 +36,35 @@ const App = () => {
       window.localStorage.setItem('loggedUser', JSON.stringify(loggedUser))
     } catch (error) {
       console.log(error)
-      window.alert('wrong credentials')
+      setNotificationMessage(({ message: "wrong username or password", isError: true }))
+      setTimeout(() => {
+        setNotificationMessage({ message: null, isError: null })
+      }, 2000)
     }
   }
 
   const handleLogout = async () => {
     window.localStorage.removeItem('loggedUser')
+    setNotificationMessage(({ message: `user ${user.username} logged out`, isError: false }))
+    setTimeout(() => {
+      setNotificationMessage({ message: null, isError: null })
+    }, 2000)
     setUser(null)
   }
 
   const postBlog = async (blog) => {
     const response = await blogService.postBlog(blog)
+    setNotificationMessage(({ message: `new blog ${response.title} by ${response.author} added`, isError: false }))
+    setTimeout(() => {
+      setNotificationMessage({ message: null, isError: null })
+    }, 2000)
     setBlogs(blogs.concat(response))
   }
 
-  return user ? <BlogView blogs={blogs} name={user && user.name} handleLogout={handleLogout} postBlog={postBlog} /> : <LoginForm handleLogin={handleLogin} />
+  return <>
+    {notificationMessage.message && <Notification message={notificationMessage.message} isError={notificationMessage.isError} />}
+    {user ? <BlogView blogs={blogs} name={user && user.name} handleLogout={handleLogout} postBlog={postBlog} /> : <LoginForm handleLogin={handleLogin} />}
+  </>
 }
 
 export default App
