9ddf618f3e8eb872ddcc58871bdffbcfc8496d29
otsohellman <otso.hellman@gmail.com>
2020-10-30 12:51:46 +0200

    5-9

--- osa5/bloglist-frontend/src/components/BlogList.js
+++ osa5/bloglist-frontend/src/components/BlogList.js
@@ -2,7 +2,9 @@ import React from 'react'
 import Blog from './Blog'
 
 export default ({ blogs, postLike }) => <div>
-    {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} postLike={postLike} />
-    )}
+    {[...blogs]
+        .sort((blogA, blogB) => blogB.likes - blogA.likes)
+        .map(blog =>
+            <Blog key={blog.id} blog={blog} postLike={postLike} />
+        )}
 </div>
