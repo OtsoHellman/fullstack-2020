8f100ab873cc513ed35ea4ac99bf0a413e1c5615
otsohellman <otso.hellman@gmail.com>
2020-11-18 16:25:49 +0200

    6-11

--- osa6/redux-anecdotes/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -2,12 +2,18 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 import { voteAnecdote } from '../reducers/anecdoteReducer'
+import { setNotification, clearNotification } from '../reducers/notificationReducer'
 const AnecdoteList = () => {
   const anecdotes = useSelector(state => state.anecdotes)
   const dispatch = useDispatch()
 
   const vote = (id) => {
+    const anecdoteContent = anecdotes.find(anecdote => anecdote.id === id).content
     dispatch(voteAnecdote(id))
+    dispatch(setNotification(`you voted '${anecdoteContent}'`))
+    setTimeout(() => {
+      dispatch(clearNotification())
+    }, (5000));
   }
 
   return <div>
--- osa6/redux-anecdotes/src/reducers/notificationReducer.js
+++ osa6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,7 +1,26 @@
-const initialState = "oispa kaljaa"
+const initialState = null
 
 const reducer = (state = initialState, action) => {
-  return state
+  switch (action.type) {
+    case 'SET_NOTIFICATION':
+      return action.data.content
+    case 'CLEAR_NOTIFICATION':
+      return null
+    default:
+      return state
+  }
 }
+
+
+export const setNotification = (content) => ({
+  type: 'SET_NOTIFICATION',
+  data: {
+    content
+  }
+})
+
+export const clearNotification = () => ({
+  type: 'CLEAR_NOTIFICATION'
+})
 
 export default reducer
