24765de649bcb9d4bd04b0db1806860ff1a8e9d5
otsohellman <otso.hellman@gmail.com>
2020-11-18 15:51:03 +0200

    6-8

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -25,8 +25,6 @@ const sortedAnecdotes = anecdotes => {
 }
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
   switch (action.type) {
     case 'VOTE':
       const id = action.data.id
