f3622ca7dbd42312df4b3898d12fe6ff186fb8a7
otsohellman <otso.hellman@gmail.com>
2020-11-18 15:48:16 +0200

    6-7

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { voteAnecdote, createAnecdote } from './reducers/anecdoteReducer'
+
+import { voteAnecdote } from './reducers/anecdoteReducer'
+
+import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
@@ -11,10 +14,6 @@ const App = () => {
     dispatch(voteAnecdote(id))
   }
 
-  const create = event => {
-    event.preventDefault()
-    dispatch(createAnecdote(event.target.anecdote.value))
-  }
 
   return (
     <div>
@@ -30,11 +29,7 @@ const App = () => {
           </div>
         </div>
       )}
-      <h2>create new</h2>
-      <form onSubmit={create}>
-        <div><input name="anecdote"/></div>
-        <button type="submit">create</button>
-      </form>
+      <AnecdoteForm />
     </div>
   )
 }
---
+++ osa6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { createAnecdote } from '../reducers/anecdoteReducer'
+
+const AnecdoteForm = () => {
+    const dispatch = useDispatch()
+
+    const create = event => {
+        event.preventDefault()
+        dispatch(createAnecdote(event.target.anecdote.value))
+    }
+
+    return <div>
+        <h2>create new</h2>
+        <form onSubmit={create}>
+            <div><input name="anecdote" /></div>
+            <button type="submit">create</button>
+        </form>
+    </div>
+}
+
+
+export default AnecdoteForm
