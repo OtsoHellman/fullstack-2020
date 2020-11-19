cb667a11ea67c1a446734c41d93258d9abda82a4
otsohellman <otso.hellman@gmail.com>
2020-11-19 17:21:28 +0200

    7-2

--- osa7/routed-anecdotes/src/App.js
+++ osa7/routed-anecdotes/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import { Switch, Route, Link } from "react-router-dom"
+import { Switch, Route, Link, useRouteMatch } from "react-router-dom"
+import Anecdote from './components/Anecdote'
 
 const Menu = () => {
   const padding = {
@@ -18,7 +19,13 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => (
+        <li key={anecdote.id}>
+          <Link to={`/anecdotes/${anecdote.id}`} >
+            {anecdote.content}
+          </Link>
+        </li>
+      ))}
     </ul>
   </div>
 )
@@ -123,11 +130,16 @@ const App = () => {
     setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
   }
 
+  const match = useRouteMatch("/anecdotes/:id")
+  const anecdote = match ? anecdotes.find(anecdote => anecdote.id === match.params.id) : null
   return (
     <div>
       <h1>Software anecdotes</h1>
       <Menu />
       <Switch>
+        <Route path="/anecdotes/:id">
+          <Anecdote anecdote={anecdote} />
+        </Route>
         <Route path="/about">
           <About />
         </Route>
