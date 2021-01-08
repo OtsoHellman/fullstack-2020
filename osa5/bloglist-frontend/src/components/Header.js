9a9b07ef2a689d1fff37799612f3e08109e04690
Otso <otso.hellman@gmail.com>
2021-01-08 11:19:32 +0200

    7-16

--- osa5/bloglist-frontend/src/components/Header.js
+++ osa5/bloglist-frontend/src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Header = ({ user, handleLogout }) => (
-    <>
-        <h2>blogs</h2>
-        {user.name}
+const Header = ({ user, handleLogout }) => <div>
+    <Link to="/">Blogs</Link>
+    <Link to="/users">Users</Link>
+    {user.name} logged in
         <button onClick={handleLogout}>log out</button>
-    </>
-)
+</div>
 
 export default Header
